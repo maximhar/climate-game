987b758775dc816a82c2e583bf13d0fb6698fe97
Maxim Harizanov <maxim.harizanov@gmail.com>
2017-01-04 02:26:54 +0200

    Add a third parameter to ExponentialModifier
    And some explanations.

--- ClimateGame/ExponentialModifier.cs
+++ ClimateGame/ExponentialModifier.cs
@@ -8,18 +8,23 @@ namespace ClimateGame
 {
     class ExponentialModifier : IPopulationModifier
     {
+        // Linear coefficient
         private double k;
+        // Exponential coefficient
         private double k2;
+        // Offset (shifts the function left/right, but leaves slope shape the same)
+        private double o;
         private UInt32 startAge;
         private UInt32 endAge;
 
         public string Name { get; }
 
-        public ExponentialModifier(String name, double k, double k2, UInt32 startAge = 0, UInt32 endAge = UInt32.MaxValue)
+        public ExponentialModifier(String name, double k, double k2, double o, UInt32 startAge = 0, UInt32 endAge = UInt32.MaxValue)
         {
             Name = Name;
             this.k = k;
             this.k2 = k2;
+            this.o = o;
             this.startAge = startAge;
             this.endAge = endAge;
         }
@@ -27,7 +32,7 @@ namespace ClimateGame
         public Generation ModifyGeneration(Generation gen)
         {
             double rate = (gen.Age >= startAge && gen.Age < endAge) ? 
-                k * Math.Exp(k2 * gen.Age - startAge) : .0;
+                k * Math.Exp(k2 * (gen.Age - o)) : .0;
             double change = gen.Count * rate;
             return gen.AddCount(change);
         }
